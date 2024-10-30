e8f39bfa1800a0e9aacb7fc0cde545b6e669b739
DanielGarcia-201901103 <2995284560101@ingenieria.usac.edu.gt>
2024-10-30 17:09:35 -0600

    Sí funciona gRCP entre golang, falta probar rust

--- Proyecto2/servidoresDisciplinas/atletismo/atletismo.go
+++ Proyecto2/servidoresDisciplinas/atletismo/atletismo.go
@@ -7,50 +7,38 @@ import (
 	"net"
 	"time"
 
-	pb "path/to/proto/student"
+	pb "myservers/paqueteProto"
 
-	"github.com/segmentio/kafka-go"
 	"google.golang.org/grpc"
 )
 
 const (
-	port        = ":8083"
-	kafkaBroker = "localhost:9092" // Cambia esto a la dirección de tu broker de Kafka
+	port = ":8083"
 )
 
 type server struct {
 	pb.UnimplementedStudentServiceServer
 }
 
-func (s *server) SubmitStudent(ctx context.Context, req *pb.StudentRequest) (*pb.StudentResponse, error) {
+func (s *server) SendStudent(ctx context.Context, req *pb.StudentRequest) (*pb.StudentResponse, error) {
 	isWinner := determineWinner()
-	topic := "winners"
+	status := "Winner"
 	if !isWinner {
-		topic = "losers"
+		status = "Not a Winner"
 	}
 
-	// Enviar a Kafka
-	writeToKafka(topic, req.Student)
+	// Imprimir información recibida en lugar de enviar a Kafka
+	log.Printf("Received student: %s, Age: %d, Faculty: %s, Discipline: %d", req.Student, req.Age, req.Faculty, req.Discipline)
+	log.Printf("Determined status: %s", status)
 
-	return &pb.StudentResponse{IsWinner: isWinner}, nil
+	// Devolver la respuesta con el estado
+	return &pb.StudentResponse{Status: status}, nil
 }
 
 // Función de probabilidad para determinar si es ganador
 func determineWinner() bool {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(2) == 0
-}
-
-func writeToKafka(topic, student string) {
-	writer := kafka.Writer{
-		Addr:  kafka.TCP(kafkaBroker),
-		Topic: topic,
-	}
-	defer writer.Close()
-	msg := kafka.Message{Value: []byte(student)}
-	if err := writer.WriteMessages(context.Background(), msg); err != nil {
-		log.Printf("Error al enviar a Kafka: %v", err)
-	}
 }
 
 func main() {
