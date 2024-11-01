34777f34dbed30b6071cd3dc8c546bc5f36ac90d
DanielGarcia-201901103 <2995284560101@ingenieria.usac.edu.gt>
2024-10-31 23:24:59 -0600

    Subiendo archivos Dockerfile.

--- Proyecto2/tconsumers/consumer2/consumer2.go
+++ Proyecto2/tconsumers/consumer2/consumer2.go
@@ -5,16 +5,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/segmentio/kafka-go"
 )
 
 const (
 	kafkaBroker = "localhost:9092"
 	topic       = "losers"
+	redisAddr   = "localhost:6379"
 )
 
+var ctx = context.Background()
+
 func main() {
+	// Configurar conexión a Redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+
+	defer rdb.Close()
+
+	// Configurar conexión a Kafka
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{kafkaBroker},
 		Topic:   topic,
@@ -40,5 +53,13 @@ func main() {
 
 		// Imprimir la información recibida
 		fmt.Printf("Perdedor recibido de Kafka: %v\n", studentInfo)
+
+		// Almacenar en Redis usando hashes
+		faculty := studentInfo["faculty"].(string)
+		discipline := strconv.Itoa(int(studentInfo["discipline"].(float64)))
+
+		// Guardar el conteo de alumnos por facultad y disciplina en Redis
+		rdb.HIncrBy(ctx, "losers_by_faculty", faculty, 1)
+		rdb.HIncrBy(ctx, "losers_by_discipline", discipline, 1)
 	}
 }
